8717ed9f54dec229f797e7bdce966efb447ffb5f
ziaoang <ziaoang@gmail.com>
2017-06-09 19:37:12 +0800

    change path

--- db.py
+++ db.py
@@ -17,8 +17,7 @@ app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:webkdd@localhost/wipweb"
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:icstwip@localhost/wipweb"
 app.config["SECRET_KEY"] = "WIPWEB_SECRET_KEY"
-# app.config["UPLOAD_URL"] = "static/upload/"
-app.config["UPLOAD_URL"] = "/var/www/wipweb-api/static/upload/"
+app.config["UPLOAD_URL"] = "static/upload/"
 
 
 @app.errorhandler(404)
