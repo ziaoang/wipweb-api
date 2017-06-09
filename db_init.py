41a7dec8d9273b3f20fcd5530739a78f24da6b64
ziaoang <ziaoang@gmail.com>
2017-06-09 17:43:05 +0800

    add admin account

---
+++ db_init.py
@@ -0,0 +1,24 @@
+# encoding=utf-8
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+
+from db import db, User
+
+
+def init_user():	
+	admin_username = "admin"
+	admin_password = "icstwip"
+	user = User()
+	user.username      = admin_username
+	user.password_hash = User.encrypt(admin_username)
+	db.session.add(user)
+	db.session.commit()
+
+
+if __name__ == "__main__":
+	init_user()
+
+
